4e6660e97e4d320e0c6f837c9d6f23b6e997c05e
HuJJ-NB <91231458+HuJJ-NB@users.noreply.github.com>
2022-03-04 10:57:15 +0800

    [add]新增了全局数据结构存储系统调用情况信息；实现每次系统调用输出TRACE级别统计数据。

--- os/src/syscall/mod.rs
+++ os/src/syscall/mod.rs
@@ -1,13 +1,41 @@
+const SYSCALL_COUNTS: usize = 128;
 const SYSCALL_WRITE: usize = 64;
 const SYSCALL_EXIT: usize = 93;
 
 mod fs;
 mod process;
 
+use crate::sync::UPSafeCell;
 use fs::*;
 use process::*;
+use lazy_static::*;
+
+struct SyscallManager {
+	ever_syscalls_count: [usize; SYSCALL_COUNTS],
+	all_syscalls_count: usize,
+}
+
+impl SyscallManager {
+	pub fn counting (&mut self, syscall_id: usize) -> (usize, usize) {
+		self.ever_syscalls_count[syscall_id] += 1;
+		self.all_syscalls_count += 1;
+		(self.ever_syscalls_count[syscall_id], self.all_syscalls_count)
+	}
+}
+lazy_static! {
+	static ref SYSCALL_MANAGER: UPSafeCell<SyscallManager> = unsafe { UPSafeCell::new(
+		SyscallManager {
+			ever_syscalls_count: [0; SYSCALL_COUNTS],
+			all_syscalls_count: 0
+		}
+	)};
+}
 
 pub fn syscall(syscall_id: usize, args: [usize; 3], user_sp: usize) -> isize {
+	let mut syscall_manager = SYSCALL_MANAGER.exclusive_access();
+	let (ever_count, all_count) = syscall_manager.counting(syscall_id);
+	drop(syscall_manager);
+	trace!("call SYSCALL_{} {} times, in totaly {} syscalls.", syscall_id, ever_count, all_count);
 	match syscall_id {
 		SYSCALL_WRITE => sys_write(args[0], args[1] as *const u8, args[2], user_sp),
 		SYSCALL_EXIT => sys_exit(args[0] as i32),
