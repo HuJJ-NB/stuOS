7c17f93560a1b99625f7f9dfbbdc52fa815799a9
HuJJ <hduhujiajun@outlook.com>
2022-03-07 20:18:44 +0800

    [mod][add]添加了新系统调用获取当前进程号；修改了获取系统调用情况的系统调用的调用函数名，使用户程序的编写更加易懂。

--- user/src/syscall.rs
+++ user/src/syscall.rs
@@ -1,7 +1,8 @@
 use core::arch::asm;
-const SYSCALL_COUNT: usize = 0;
+const SYSCALL_GETSYSCALLINFO: usize = 0;
 const SYSCALL_WRITE: usize = 64;
 const SYSCALL_EXIT: usize = 93;
+const SYSCALL_GETTASKINFO: usize = 94;
 
 fn syscall(id: usize, args: [usize; 3]) -> isize {
 	let mut ret: isize;
@@ -17,8 +18,8 @@ fn syscall(id: usize, args: [usize; 3]) -> isize {
 	ret
 }
 
-pub fn sys_count(syscall_id: usize) -> isize {
-	syscall(SYSCALL_COUNT, [syscall_id, 0, 0])
+pub fn sys_get_syscallinfo(syscall_id: usize) -> isize {
+	syscall(SYSCALL_GETSYSCALLINFO, [syscall_id, 0, 0])
 }
 
 pub fn sys_write(fd: usize, buffer: &[u8]) -> isize {
@@ -27,4 +28,8 @@ pub fn sys_write(fd: usize, buffer: &[u8]) -> isize {
 
 pub fn sys_exit(exit_code: i32) -> isize {
 	syscall(SYSCALL_EXIT, [exit_code as usize, 0, 0])
+}
+
+pub fn sys_get_taskinfo() -> isize {
+	syscall(SYSCALL_GETTASKINFO, [0, 0, 0])
 }
